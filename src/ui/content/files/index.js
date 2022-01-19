066a2bd494ffa6f67a75fdb635eec3f5c9162f41
rootxdwt <65634206+rootxdwt@users.noreply.github.com>
2022-01-19 10:26:54 +0900

    added selectbox(need repair)

--- src/ui/content/files/index.js
+++ src/ui/content/files/index.js
@@ -5,59 +5,110 @@ export class Files extends React.Component {
   constructor(props) {
     super(props);
     this.clickDetectionArea = React.createRef();
-    this.state = { selectedIndex: [] };
+    this.state = { selectedIndex: [], startPos: [0, 0], currentPos: [0, 0] };
   }
   render() {
     var selectedStyle = { backgroundColor: "rgba(255,255,255,0.1)" };
     return (
-      <>
+      <div ref={this.clickDetectionArea}>
+        <div
+          className="dragSquare"
+          style={{
+            top:
+              this.state.currentPos[1] - this.state.startPos[1] > 0
+                ? this.state.startPos[1]
+                : this.state.currentPos[1],
+
+            left:
+              this.state.currentPos[0] - this.state.startPos[0] > 0
+                ? this.state.startPos[0]
+                : this.state.currentPos[0],
+            width:
+              this.state.currentPos[0] - this.state.startPos[0] >= 0
+                ? this.state.currentPos[0] - this.state.startPos[0]
+                : (this.state.currentPos[0] - this.state.startPos[0]) * -1,
+            height:
+              this.state.currentPos[1] - this.state.startPos[1] >= 0
+                ? this.state.currentPos[1] - this.state.startPos[1]
+                : (this.state.currentPos[1] - this.state.startPos[1]) * -1,
+          }}
+        ></div>
         <div className="fileControlOptions"></div>
-        <div style={{ height: "100vh" }} ref={this.clickDetectionArea}>
-          <div
-            className="fileContainer"
-            style={
-              this.state.selectedIndex.indexOf(1) != -1 ? selectedStyle : {}
-            }
-            objectid="89shHGfdasg"
-            index={1}
-          >
-            a
-          </div>
-          <div
-            className="fileContainer"
-            style={
-              this.state.selectedIndex.indexOf(2) != -1 ? selectedStyle : {}
-            }
-            objectid="8HBF579hfc"
-            index={2}
-          >
-            a
-          </div>
-          <div
-            className="fileContainer"
-            style={
-              this.state.selectedIndex.indexOf(3) != -1 ? selectedStyle : {}
-            }
-            objectid="8Gvxzjaskdu"
-            index={3}
-          >
-            a
-          </div>
+        <div style={{ height: "100vh" }}>
+          {[
+            "89shHGfdasg",
+            "8HBF579hfcX",
+            "8Gvxzjaskdu",
+            "lW6PdOYPNCW",
+            "1lNa9ADW58H",
+            "7UdbAm8LGtm",
+            "JTn0psPPtDs",
+            "slCVk11xUV2",
+            "rg2avRUQgpr",
+          ].map((id, index) => {
+            return (
+              <div
+                className="fileContainer"
+                style={
+                  this.state.selectedIndex.indexOf(index + 1) != -1
+                    ? selectedStyle
+                    : {}
+                }
+                objectid={id}
+                key={id}
+                data-index={index + 1}
+              >
+                a
+              </div>
+            );
+          })}
         </div>
-      </>
+      </div>
     );
   }
 
-  getIdFromIndex(index) {
-    return document.querySelector(`[index="${index}"]`).getAttribute("objectid")
-  }
+  getIdFromIndex = (index) => {
+    return document
+      .querySelector(`[data-index="${index}"]`)
+      .getAttribute("objectid");
+  };
 
-  componentDidMount() {
-    this.clickDetectionArea.current.addEventListener("click", (e) => {
-      var targetIndex = parseInt(e.target.getAttribute("index"));
-      if (targetIndex == "") {
+  mouseMove = (mouseMoveEvent) => {
+    this.setState({
+      currentPos: [mouseMoveEvent.clientX, mouseMoveEvent.clientY],
+    });
+    if (mouseMoveEvent.target.getAttribute("data-index")) {
+      if (
+        this.state.selectedIndex.indexOf(
+          parseInt(mouseMoveEvent.target.getAttribute("data-index"))
+        ) == -1
+      ) {
+        this.setState({
+          selectedIndex: this.state.selectedIndex.concat([
+            parseInt(mouseMoveEvent.target.getAttribute("data-index")),
+          ]),
+        });
+      }
+    }
+  };
+
+  componentDidMount = () => {
+    this.clickDetectionArea.current.addEventListener("mousedown", (e) => {
+      this.setState({ startPos: [0, 0], currentPos: [0, 0] });
+      var targetIndex = e.target.getAttribute("data-index");
+      if (targetIndex == null) {
+        //dragsquare
+        this.setState({ startPos: [e.clientX, e.clientY] });
+        this.mouseMove(e)
+        window.addEventListener("mousemove", this.mouseMove);
+        window.addEventListener("mouseup", () => {
+          window.removeEventListener("mousemove", this.mouseMove);
+          this.setState({ startPos: [0, 0], currentPos: [0, 0] });
+        });
         this.setState({ selectedIndex: [] });
       } else {
+        targetIndex = parseInt(targetIndex);
+        //click,shift,ctrl
         if (e.shiftKey) {
           if (this.state.selectedIndex.length == 0) {
             this.setState({
@@ -106,5 +157,5 @@ export class Files extends React.Component {
         }
       }
     });
-  }
+  };
 }

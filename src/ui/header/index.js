870a98d750617111483ab9efcf8f901fa09c66a4
rootxdwt <65634206+rootxdwt@users.noreply.github.com>
2022-04-02 11:26:04 +0900

    mobile UI improvement

--- src/ui/header/index.js
+++ src/ui/header/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import "./index.css";
+import {AiOutlineMenu} from 'react-icons/ai'
 
 export class Header extends React.Component {
+  menuClick = () =>{
+    console.log("a")
+  }
   render() {
     return (
       <>
         <div className="header">
+          <div className="menuBtn" onClick={()=>this.menuClick()}>
+          <AiOutlineMenu />
+          </div>
           <input type="text" placeholder="Search File, Folder"></input>{" "}
         </div>
       </>

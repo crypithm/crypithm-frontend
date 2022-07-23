8e8c17ecfba5be0d78d4ce9facb027a9e1c6a90b
rootxdwt <65634206+rootxdwt@users.noreply.github.com>
2022-07-23 22:01:20 +0900

    made video player

--- src/ui/viewer/index.js
+++ src/ui/viewer/index.js
@@ -1,34 +1,78 @@
 import React from "react";
 import "./index.css";
 import { viewableType } from "../../vars";
-import { RiArrowDropLeftLine,RiZoomInLine,RiZoomOutLine,RiFullscreenLine,RiFullscreenExitLine,RiDownload2Line  } from "react-icons/ri";
+import {
+  RiArrowDropLeftLine,
+  RiZoomInLine,
+  RiZoomOutLine,
+  RiFullscreenLine,
+  RiFullscreenExitLine,
+  RiDownload2Line,
+  RiPlayLine,
+  RiPauseLine,
+} from "react-icons/ri";
 import { getFileBlob } from "../../lib/crypto/decrypt";
-
 
 class ViewBox extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { imgSize:[window.innerWidth * 0.85, ""],isFull:false };
-    this.imgRef = React.createRef()
+    this.state = {
+      imgSize: [window.innerWidth * 0.85, ""],
+      isFull: false,
+      vidDuration: 0,
+      playing: false,
+      vidPos: 0,
+    };
+    this.imgRef = React.createRef();
+    this.vidref = React.createRef();
+    this.progressbarRef = React.createRef();
+    this.isVideoPosChanging = false;
+    console.log(this.props.fullMime);
   }
-
-
   imgScaleChange = async (zoomin) => {
     if (!zoomin) {
-      this.setState({ imgSize:[this.state.imgSize[0] * 0.75, ""] });
+      this.setState({ imgSize: [this.state.imgSize[0] * 0.75, ""] });
     } else {
-      this.setState({ imgSize:[this.state.imgSize[0] * 1.25, ""] });
+      this.setState({ imgSize: [this.state.imgSize[0] * 1.25, ""] });
     }
   };
 
-
-  setFullScreen=()=>{
-    if(!this.state.isFull){
-      this.setState({imgSize:[window.innerWidth,""],isFull:true})
-    }else{
-      this.setState({imgSize:[window.innerWidth * 0.85, ""], isFull:false})
+  setFullScreen = () => {
+    if (!this.state.isFull) {
+      this.setState({ imgSize: [window.innerWidth, ""], isFull: true });
+      document.documentElement.requestFullscreen();
+    } else {
+      this.setState({ imgSize: [window.innerWidth * 0.85, ""], isFull: false });
+      document.exitFullscreen();
     }
-  }
+  };
+  playPause = () => {
+    if (this.state.playing) {
+      this.setState({ playing: false });
+      this.vidref.current.pause();
+    } else {
+      this.setState({ playing: true });
+      this.vidref.current.play();
+    }
+  };
+  modifyprogress = () => {
+    console.log(this.vidref.current.currentTime);
+  };
+  videoStateChanged = () => {
+    this.setState({
+      vidPos:
+        (this.vidref.current.currentTime / this.vidref.current.duration) * 100,
+    });
+  };
+  changeVidProg = (e) => {
+    if (this.isVideoPosChanging) {
+      this.vidref.current.currentTime =
+        this.vidref.current.duration *
+        ((e.clientX -
+          this.progressbarRef.current.getBoundingClientRect().left) /
+          this.progressbarRef.current.clientWidth);
+    }
+  };
   render() {
     switch (this.props.fullMime.split("/")[0]) {
       case "image":
@@ -41,28 +85,87 @@ class ViewBox extends React.Component {
               ref={this.imgRef}
               className="viewerImage"
             />
-            <div className="imgControl" style={{display:this.props.showControls?"":"none"}}>
+            <div
+              className="imgControl"
+              style={{ display: this.props.showControls ? "" : "none" }}
+            >
               <div className="focus">
-                <div className="eachBtn" onClick={()=>this.imgScaleChange(true)}>
-                <RiZoomInLine />
+                <div
+                  className="eachBtn"
+                  onClick={() => this.imgScaleChange(true)}
+                >
+                  <RiZoomInLine />
                 </div>
-                <div className="eachBtn" onClick={()=>this.imgScaleChange(false)}>
-                <RiZoomOutLine />
+                <div
+                  className="eachBtn"
+                  onClick={() => this.imgScaleChange(false)}
+                >
+                  <RiZoomOutLine />
                 </div>
                 <div className="eachBtn" onClick={this.setFullScreen}>
-                {this.state.isFull?<RiFullscreenExitLine />:<RiFullscreenLine />}
+                  {this.state.isFull ? (
+                    <RiFullscreenExitLine />
+                  ) : (
+                    <RiFullscreenLine />
+                  )}
                 </div>
               </div>
             </div>
           </>
         );
       case "application":
-        console.log(this.props.fullMime)
-        if(this.props.fullMime.split("/")[1]=="pdf"){
-          return(
-            <></>
-          )
+        if (this.props.fullMime.split("/")[1] == "pdf") {
+          return <></>;
         }
+      case "video":
+        return (
+          <>
+            <div className="vidElem">
+              <video
+                type={this.props.fullMime}
+                controls={false}
+                width={this.state.imgSize[0]}
+                height={this.state.imgSize[1]}
+                ref={this.vidref}
+                onTimeUpdate={this.videoStateChanged}
+              >
+                <source src={this.props.src}></source>
+              </video>
+              <div
+                className="vidControlbar"
+                style={{ display: this.props.showControls ? "" : "none" }}
+              >
+                <div className="playPause" onClick={this.playPause}>
+                  {this.state.playing ? <RiPauseLine /> : <RiPlayLine />}
+                </div>
+                <div className="vidProgress">
+                  <progress
+                    onMouseDown={(e) => {
+                      this.isVideoPosChanging = true;
+                    }}
+                    onMouseUp={() => {
+                      this.isVideoPosChanging = false;
+                    }}
+                    onMouseLeave={() => {
+                      this.isVideoPosChanging = false;
+                    }}
+                    onMouseMove={this.changeVidProg}
+                    value={this.state.vidPos}
+                    max={100}
+                    ref={this.progressbarRef}
+                  ></progress>
+                </div>
+                <div className="fullScreen" onClick={this.setFullScreen}>
+                  {this.state.isFull ? (
+                    <RiFullscreenExitLine />
+                  ) : (
+                    <RiFullscreenLine />
+                  )}
+                </div>
+              </div>
+            </div>
+          </>
+        );
     }
   }
 }
@@ -70,22 +173,23 @@ class ViewBox extends React.Component {
 export class Viewer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { sourceUrl: "", type: "",  mime:"", showAddition: true };
+    this.state = { sourceUrl: "", type: "", mime: "", showAddition: true };
   }
   componentDidMount = async () => {
     var [blobSource, mime] = await getFileBlob(this.props.id, this.props.name);
-    var prevtoid=""
+    var prevtoid = "";
     var tf = true;
     if (viewableType.indexOf(mime) == -1) {
       tf = false;
     }
-    this.setState({ sourceUrl: blobSource, isViewable: tf, mime:mime });
-    window.addEventListener("mousemove",()=>{
-      this.setState({showAddition:true})
-      clearTimeout(prevtoid)
-      prevtoid = setTimeout(()=>{this.setState({showAddition:false})},3000)
-    })
-
+    this.setState({ sourceUrl: blobSource, isViewable: tf, mime: mime });
+    window.addEventListener("mousemove", () => {
+      this.setState({ showAddition: true });
+      clearTimeout(prevtoid);
+      prevtoid = setTimeout(() => {
+        this.setState({ showAddition: false });
+      }, 3000);
+    });
   };
 
   download() {
@@ -98,12 +202,23 @@ export class Viewer extends React.Component {
     return (
       <>
         <div className="ViewArea">
-          <div className={`viewHeader ${this.state.showAddition ? "" : "hiddenvh"}`}>
+          <div
+            className={`viewHeader ${
+              this.state.showAddition ? "" : "hiddenvh"
+            }`}
+          >
             <p className="close" onClick={() => this.props.close()}>
               <RiArrowDropLeftLine />
             </p>
             <p className="fileName">{this.props.name}</p>
-            {this.state.sourceUrl !=""?<div className="downloadBtn" onClick={() => this.download()}><RiDownload2Line /><p>다운로드</p></div>:<></>}
+            {this.state.sourceUrl != "" ? (
+              <div className="downloadBtn" onClick={() => this.download()}>
+                <RiDownload2Line />
+                <p>다운로드</p>
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
           <div className="viewBody">
             {this.state.sourceUrl != "" ? (
